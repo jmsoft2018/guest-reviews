28b14ea98eb15a4c11b31cfb6b67b9bb5f5ce2fd
z.annamuratov <annamuratov1987@gmail.com>
2020-02-02 10:46:34 +0500

    commit

---
+++ guest-reviews/migrations/2020_02_02_042754_create_guests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gr_guests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_agent')->unique();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gr_guests', function (Blueprint $table) {
+            Schema::dropIfExists('gr_guests');
+        });
+    }
+}
